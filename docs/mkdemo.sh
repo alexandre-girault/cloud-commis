145abb0daba94028de8a0b9feb1fa42220deb445
Alex <alexandre.girault@gmail.com>
2025-02-08 18:55:09 +0100

    update demo

--- docs/mkdemo.sh
+++ docs/mkdemo.sh
@@ -9,10 +9,9 @@ TYPES="t2.micro t4g.nano m6g.xlarge c5a.large"
 
 # static random vmlist html
 echo '<div class="container-fluid">
-<table class="table table-hover" id="vmlist-table">
+<table class="table table-hover" id="vmlist-table" style="font-size: 12px;">
 <thead>
   <tr>
-    <th></th>
     <th>Region</th>
     <th>AwsAccountId</th>
     <th>InstanceId</th>
@@ -20,7 +19,8 @@ echo '<div class="container-fluid">
 	  <th>Architecture</th>
 	  <th>PlatformDetails</th> 
 	  <th>InstanceType</th>
-	  <th>State</th>       
+	  <th>State</th>
+    <th>Details</th>   
   </tr>
 </thead>
 <tbody>' > vmlist
@@ -41,13 +41,6 @@ do
     fi
 
     echo "<tr>
-        <td>
-          <div class=\"form-check\">
-          <input class=\"form-check-input\" type=\"radio\" name=\"flexRadio\" id=\"flexRadioDefault${ID}\"
-           hx-get=\"/cloud-commis/vmdetails-${ARCH}\"
-           hx-trigger=\"click\" hx-target=\"#vmdetails\" hx-swap=\"innerHTML\">
-         </div>
-       </td>
        <td>${REGION}</td>
        <td>${ACCOUNT}</td>
        <td>${ID}</td>
@@ -56,6 +49,13 @@ do
 	   <td>Linux/UNIX</td>  
 	   <td>${TYPE}</td>
 	   <td>${STATE}</td>
+     <td>
+       <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"
+         hx-get="/cloud-commis/vmdetails"
+         hx-trigger="click" hx-target="#rightPanelDetails" hx-swap="innerHTML">
+         Details
+       </button>
+     </td>
        </tr>" >> vmlist
 done
 
@@ -68,92 +68,44 @@ echo "</tbody>
 \$('#vmlist-table').DataTable();
 </script>
 <div id=vmdetails class=\"border border-secondary text-bg-light d-flex dropdown\" >
-</div>" >> vmlist
+</div>">> vmlist
+
+echo '<div class="offcanvas offcanvas-end" data-bs-scroll="true" 
+data-bs-backdrop="false" tabindex="-1" 
+id="offcanvasRight" aria-labelledby="offcanvasRightLabel" >
+  <div class="offcanvas-header">
+    <h5 id="offcanvasRightLabel">vm Details</h5>
+    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+  </div>
+  <div class="offcanvas-body">
+    <!-- style="overflow:auto; width: 350px;" -->
+    <pre class="bg-opacity-10 bg-warning " id="rightPanelDetails" style="white-space: pre-wrap;">
+    </pre>
+  </div>
+</div>'>> vmlist
 
 
 # static  vmdetails-arm_64 html
-echo '<div class="container p-2 d-flex justify-content-center">
-<div class="row overflow-auto gy-5">
-
-    <div class="col"> 
-Instance details :
-    <pre class="p-3 col border bg-opacity-10 bg-warning" style="width:600px; height: 400px;">
-
+echo '
 {
-  "Name": "test",
+  "Name": "demo values",
   "Architecture": "arm64",
   "LaunchTime": "2024-10-26T13:15:55Z",
   "UsageOperationUpdateTime": "2024-10-26T13:15:55Z",
   "PlatformDetails": "Linux/UNIX",
-  "ImageId": "ami-01c5300f289d64643",
-  "InstanceType": "t4g.nano",
+  "ImageId": "ami-01c5300f289d6XXXX",
+  "InstanceType": "xxx.someSize",
   "PublicIpAddress": "",
-  "State": "stopped",
+  "State": "running",
   "KernelImage": "Kernel 6.1.112-122.189.amzn2023.aarch64 on an aarch64 (-)\r",
   "BootImage": "Booting `Amazon Linux (6.1.112-122.189.amzn2023.aarch64) 2023"
 }
 
-    </pre>
-    </div>
-
-    <div class="col">
-Image details :
-    <pre class="p-3 col border bg-opacity-10 bg-warning"" style="width:600px; height: 400px;">
-
 {
   "Name": "al2023-ami-2023.6.20241010.0-kernel-6.1-arm64",
-  "Region": "eu-west-3",
+  "Region": "xx-west-3",
   "Description": "Amazon Linux 2023 AMI 2023.6.20241010.0 arm64 HVM kernel-6.1",
   "OwnerId": "137112412989",
   "DeprecationTime": "2025-01-09T16:54:00.000Z"
 }
-
-    </pre>
-    </div>
-
-  </div>
-</div>' > vmdetails-arm_64
-
-# static  vmdetails-x86_64 html
-echo '
-<div class="container p-2 d-flex justify-content-center">
-  <div class="row overflow-auto gy-5">
-
-    <div class="col"> 
-Instance details :
-    <pre class="p-3 col border bg-opacity-10 bg-warning" style="width:600px; height: 400px;">
-
-{
-  "Name": "test2",
-  "Architecture": "x86_64",
-  "LaunchTime": "2024-10-26T13:31:35Z",
-  "UsageOperationUpdateTime": "2024-10-26T13:31:35Z",
-  "PlatformDetails": "Linux/UNIX",
-  "ImageId": "ami-07db896e164bc4476",
-  "InstanceType": "t2.micro",
-  "PublicIpAddress": "",
-  "State": "stopped",
-  "KernelImage": "vmlinuz-6.8.0-1015-aws root",
-  "BootImage": "Welcome to \u001b[1mUbuntu 22.04.5 LTS\u001b[0m!"
-}
-
-    </pre>
-    </div>
-
-    <div class="col">
-Image details :
-    <pre class="p-3 col border bg-opacity-10 bg-warning"" style="width:600px; height: 400px;">
-
-{
-  "Name": "ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-20240927",
-  "Region": "eu-west-3",
-  "Description": "Canonical, Ubuntu, 22.04 LTS, amd64 jammy image build on 2024-09-27",
-  "OwnerId": "099720109477",
-  "DeprecationTime": "2026-09-27T03:11:33.000Z"
-}
-
-    </pre>
-    </div>
-
-  </div>
-</div>' > vmdetails-x86_64
+' > vmdetails
